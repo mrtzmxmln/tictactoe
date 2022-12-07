8bebfd77bf1b667598266696595b25bcfc5a7033
Moritz Meyer <moritzmaximilian@gmx.net>
2022-12-08 00:54:18 +0100

    .

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 let player = 0;
 let playCount = 1;
+let gameStatus = 0; // 0 = playing, 1 = nobody wins, 2 = red wins, 3 = orange wins
+let gameField = [
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0
+            ];
 
 const fieldList = document.querySelectorAll(".field");
 const plays = document.querySelector(".plays");
-const winner = document.querySelector(".winner");
+const statusMessage = document.querySelector(".statusMessage");
 
 function onClick(field) {
-    if(player == 0) {
-        fieldList[field].style.backgroundColor = "red";
-        player = 1;
-        updateStats();
-    } else if(player == 1) {
-        fieldList[field].style.backgroundColor = "orange";
-        player = 0;
-        updateStats();
+    if(gameField[field] == 0) {
+        if(player == 0) {
+            fieldList[field].style.backgroundColor = "red";
+            player = 1;
+            updateStats();
+        } else if(player == 1) {
+            fieldList[field].style.backgroundColor = "orange";
+            player = 0;
+            updateStats();
+        }
+        playCount++;
+        checkWin();
     }
-    playCount++;
-    checkWin();
+    gameField[field] = 1;
 }
 
 function updateStats() {
@@ -24,55 +33,61 @@ function updateStats() {
 }
 
 function checkWin() {
-    /* ROT */
-    /* horizontal */
-    if((fieldList[0].style.backgroundColor == "red") && (fieldList[1].style.backgroundColor == "red") && (fieldList[2].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    } else if((fieldList[3].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[5].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    } else if((fieldList[6].style.backgroundColor == "red") && (fieldList[7].style.backgroundColor == "red") && (fieldList[8].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    } 
-    /* vertikal */
-    else if((fieldList[0].style.backgroundColor == "red") && (fieldList[3].style.backgroundColor == "red") && (fieldList[6].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    } else if((fieldList[1].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[7].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    } else if((fieldList[2].style.backgroundColor == "red") && (fieldList[5].style.backgroundColor == "red") && (fieldList[8].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    }
-    /* schräg */
-    else if((fieldList[0].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[8].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    } else if((fieldList[2].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[6].style.backgroundColor == "red")) {
-        winner.innerHTML = "Red";
-    }
+    // check red
+    if(
+        // horizontal
+        ((fieldList[0].style.backgroundColor == "red") && (fieldList[1].style.backgroundColor == "red") && (fieldList[2].style.backgroundColor == "red")) || 
+        ((fieldList[3].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[5].style.backgroundColor == "red")) ||
+        ((fieldList[6].style.backgroundColor == "red") && (fieldList[7].style.backgroundColor == "red") && (fieldList[8].style.backgroundColor == "red")) ||
+        // vertical
+        ((fieldList[0].style.backgroundColor == "red") && (fieldList[3].style.backgroundColor == "red") && (fieldList[6].style.backgroundColor == "red")) ||
+        ((fieldList[1].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[7].style.backgroundColor == "red")) ||
+        ((fieldList[2].style.backgroundColor == "red") && (fieldList[5].style.backgroundColor == "red") && (fieldList[8].style.backgroundColor == "red")) ||
+        // diagonaly
+        ((fieldList[0].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[8].style.backgroundColor == "red")) ||
+        ((fieldList[2].style.backgroundColor == "red") && (fieldList[4].style.backgroundColor == "red") && (fieldList[6].style.backgroundColor == "red")))
 
-    /* ORANGE */
-    /* horizontal */
-    if((fieldList[0].style.backgroundColor == "orange") && (fieldList[1].style.backgroundColor == "orange") && (fieldList[2].style.backgroundColor == "orange")) {
-        winner.innerHTML = "Orange";
-    } else if((fieldList[3].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[5].style.backgroundColor == "orange")) {
-        winner.innerHTML = "orange";
-    } else if((fieldList[6].style.backgroundColor == "orange") && (fieldList[7].style.backgroundColor == "orange") && (fieldList[8].style.backgroundColor == "orange")) {
-        winner.innerHTML = "Orange";
-    } 
-    /* vertikal */
-    else if((fieldList[0].style.backgroundColor == "orange") && (fieldList[3].style.backgroundColor == "orange") && (fieldList[6].style.backgroundColor == "orange")) {
-        winner.innerHTML = "Orange";
-    } else if((fieldList[1].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[7].style.backgroundColor == "orange")) {
-        winner.innerHTML = "Orange";
-    } else if((fieldList[2].style.backgroundColor == "orange") && (fieldList[5].style.backgroundColor == "orange") && (fieldList[8].style.backgroundColor == "orange")) {
-        winner.innerHTML = "Orange";
+        {
+            gameStatus = 1;
+        }
+
+    // check orange
+    else if(
+        // horizontal
+        ((fieldList[0].style.backgroundColor == "orange") && (fieldList[1].style.backgroundColor == "orange") && (fieldList[2].style.backgroundColor == "orange")) ||
+        ((fieldList[3].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[5].style.backgroundColor == "orange")) ||
+        ((fieldList[6].style.backgroundColor == "orange") && (fieldList[7].style.backgroundColor == "orange") && (fieldList[8].style.backgroundColor == "orange")) ||
+        // vertical
+        ((fieldList[0].style.backgroundColor == "orange") && (fieldList[3].style.backgroundColor == "orange") && (fieldList[6].style.backgroundColor == "orange")) ||
+        ((fieldList[1].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[7].style.backgroundColor == "orange")) ||
+        ((fieldList[2].style.backgroundColor == "orange") && (fieldList[5].style.backgroundColor == "orange") && (fieldList[8].style.backgroundColor == "orange")) ||
+        // diagonal
+        ((fieldList[0].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[8].style.backgroundColor == "orange")) ||
+        ((fieldList[2].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[6].style.backgroundColor == "orange")))
+        
+        {
+            gameStatus = 2;
+        }
+
+    if(gameStatus == 1) {
+        statusMessage.innerHTML = "Red Wins!";
+        disableGame();
+    } else if (gameStatus == 2) {
+        statusMessage.innerHTML = "Orange Wins!";
+        disableGame();
+    } else if((playCount == 10) && (gameStatus == 0)) {
+        statusMessage.innerHTML = "Nobody Wins!";
+        disableGame();
     }
-    /* schräg */
-    else if((fieldList[0].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[8].style.backgroundColor == "orange")) {
-        winner.innerHTML = "Orange";
-    } else if((fieldList[2].style.backgroundColor == "orange") && (fieldList[4].style.backgroundColor == "orange") && (fieldList[6].style.backgroundColor == "orange")) {
-        winner.innerHTML = "Orange";
-    } 
+}
+
+function disableGame() {
+    for(let i = 0; i < gameField.length; i++) {
+        gameField[i] = 1;
+    }
 }
 
 function resetGame() {
     location.reload();
 }
+
